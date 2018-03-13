00b1c058af704e8256c5f4773ec7508b30655ebc
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-03-13 14:18:02 +0530

    sns added

--- IssueCheck/lambda.js
+++ IssueCheck/lambda.js
@@ -1,6 +1,34 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 	//fail
-	callback(null,'Successfully executed');
+	sns.publish({
+		Message: 'this is a test message',
+		Subject: 'test',
+		MessageAttributes: {
+			'test': {
+				DataType: 'String',
+				StringValue: '123'
+			},
+			'test1': {
+				DataType: 'Number',
+				StringValue: '456'
+			},
+			'sample': {
+				DataType: 'Binary',
+				BinaryValue: '001'
+			},
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:263248768798:Testtopic'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
+	callback(null, 'Successfully executed');
 }
